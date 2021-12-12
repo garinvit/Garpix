32f9b08cddb5f0fda094fc495bb84335f23e0414
garinvit <garin.vitaly@yandex.ru>
2021-12-12 13:12:03 +0300

    add hotels

--- hotels/backend/hotels/models/hotels_page.py
+++ hotels/backend/hotels/models/hotels_page.py
@@ -1,6 +1,7 @@
 from django.db import models
 from garpix_page.models import BaseListPage
-
+from garpix_utils.paginator import GarpixPaginator
+from .one_hotel_page import Comfort, OneHotelPage, CHOICES
 
 class HotelsPage(BaseListPage):
     paginate_by = 3
@@ -13,6 +14,28 @@ class HotelsPage(BaseListPage):
 
     def get_context(self, request=None, *args, **kwargs):
         context = super().get_context(request, *args, **kwargs)
-        print(context)
-        print(request.GET)
+        types = [x[1] for x in CHOICES]
+        context.update({"comfort": Comfort.objects.all(), "types": types})
+
+        if request.GET:
+            request_set = set(request.GET.dict())
+            comfort_set = set([x.title for x in Comfort.objects.all()]).intersection(request_set)
+            types_set = [x.upper() for x in set(types).intersection(request_set)]
+            # print("comfort set", comfort_set, types_set, request_set)
+            object_list = OneHotelPage.objects.filter(type__in=types_set)
+            object_list = [x for x in object_list if comfort_set.issubset(x.get_comfort_title())]
+            paginator = GarpixPaginator(object_list, self.paginate_by)
+            try:
+                page = int(request.GET.get('page', 1))
+            except ValueError:
+                page = 1
+
+            paginated_object_list = paginator.get_page(page)
+
+            context.update({
+                'paginator': paginator,
+                'paginated_object_list': paginated_object_list,
+                'page': page,
+                'obj_len': len(object_list),
+            })
         return context
--- hotels/backend/hotels/models/one_hotel_page.py
+++ hotels/backend/hotels/models/one_hotel_page.py
@@ -1,6 +1,11 @@
 from django.db import models
 from garpix_page.models import BasePage
 
+CHOICES = (
+    ("ГОСТИНИЦА", "Гостиница"),
+    ("МОТЕЛЬ", "Мотель"),
+    ("АПАРТАМЕНТЫ", "Апартаменты"),
+)
 
 class Comfort(models.Model):
     title = models.CharField(max_length=255, verbose_name='Удобства')
@@ -13,15 +18,14 @@ class Comfort(models.Model):
     def __str__(self):
         return self.title
 
+
+
 class OneHotelPage(BasePage):
     # name = models.CharField(max_length=255, verbose_name='Название отеля')
-    type = models.CharField(max_length=255, verbose_name='Тип отеля', choices=(
-        ("ГОСТИНИЦА", "Гостиница"),
-        ("МОТЕЛЬ", "Мотель"),
-        ("АПАРТАМЕНТЫ", "Апартаменты"),
-    ))
+    type = models.CharField(max_length=255, verbose_name='Тип отеля', choices=CHOICES)
+    # type = models.ForeignKey(Type, on_delete=models.CASCADE)
     description = models.CharField(max_length=255, verbose_name='Описание', blank=True, default='')
-    comfort = models.ManyToManyField(Comfort, blank=True, verbose_name='Удобства')
+    comfort = models.ManyToManyField(Comfort, blank=True, verbose_name='Удобства', related_name="comfort")
     stars = models.PositiveIntegerField(verbose_name='Звезды', blank=True)
     rating = models.FloatField(verbose_name='Рейтинг', blank=True)
     price = models.PositiveIntegerField(verbose_name='Цена')
@@ -33,3 +37,6 @@ class OneHotelPage(BasePage):
         verbose_name = "Подробно Отель"
         verbose_name_plural = "Подробно Отели"
         ordering = ("-created_at",)
+
+    def get_comfort_title(self):
+        return set([x.title for x in self.comfort.all()])
